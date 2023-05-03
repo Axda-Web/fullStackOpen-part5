e5e3a5233f843a64c365cb87b68a45b4311c3199
Alyx DARENNE <axda.web@gmail.com>
2023-05-03 08:59:39 +0200

    part5/ex1: Add login service file

---
+++ bloglist-frontend/src/services/login.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const BASED_URL = '/api/login'
+
+const login = async (credentials) => {
+    const response = await axios.post(BASED_URL, credentials)
+    return response.data
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    login
+}
+
