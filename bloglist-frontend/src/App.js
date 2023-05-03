abba3272907d7f8f53678e9df6f197901a97efb8
Alyx DARENNE <axda.web@gmail.com>
2023-05-03 10:27:30 +0200

    part5/ex2: Add logout feature

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -48,6 +48,11 @@ const App = () => {
     }
   }
 
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
+  }
+
   return (
     <>
       { !user 
@@ -64,6 +69,7 @@ const App = () => {
         <div>
           <h2>blogs</h2>
           <p>{user.username} logged in</p>
+          <button onClick={handleLogout}>Logout</button>
           {blogs.map(blog =>
             <Blog key={blog.id} blog={blog} />
           )}
