8f967899ad5559ac8746b35a6c68cd3fada4ec08
Alyx DARENNE <axda.web@gmail.com>
2023-05-10 17:51:55 +0200

    part5/ex14: Test blog url and likes rendering

--- bloglist-frontend/src/components/Blog.test.js
+++ bloglist-frontend/src/components/Blog.test.js
@@ -1,31 +1,43 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders content', () => {
-  const blog = {
-    title: 'TDD with Jest and RTL',
-    author: 'Axda',
-    url: 'http://localhost:3003/api/blogs/21',
-    likes: 35
-  }
+describe('<Blog />', () => {
+  beforeEach(() => {
+    const blog = {
+      title: 'TDD with Jest and RTL',
+      author: 'Axda',
+      url: 'http://localhost:3003/api/blogs/21',
+      likes: 35
+    }
+    render(<Blog blog={blog} />)
+  })
 
-  /* const { container } = */ render(<Blog blog={blog} />)
-  //   const div = container.querySelector('.blog')
+  test('renders blog title and author', () => {
 
-  //   expect(div).toHaveTextContent(/TDD with Jest and RTL/)
-  //   expect(div).toHaveTextContent(/Axda/)
-  //   expect(div).not.toHaveTextContent(/http:\/\/localhost:3003\/api\/blogs\/21/)
-  //   expect(div).not.toHaveTextContent(/35/)
+    const title = screen.getByText(/TDD with Jest and RTL/)
+    const author = screen.getByText(/Axda/)
+    const url = screen.queryByText(/http:\/\/localhost:3003\/api\/blogs\/21/)
+    const likes = screen.queryByText(/35/)
 
-  const title = screen.getByText(/TDD with Jest and RTL/)
-  const author = screen.getByText(/Axda/)
-  const url = screen.queryByText(/http:\/\/localhost:3003\/api\/blogs\/21/)
-  const likes = screen.queryByText(/35/)
+    expect(title).toBeDefined()
+    expect(author).toBeDefined()
+    expect(url).toBeNull()
+    expect(likes).toBeNull()
+  })
 
-  expect(title).toBeDefined()
-  expect(author).toBeDefined()
-  expect(url).toBeNull()
-  expect(likes).toBeNull()
+  test('renders url and number of likes if view button is clicked', async () => {
+
+    const user = userEvent.setup()
+    const viewButton = screen.getByText(/view/)
+    await user.click(viewButton)
+
+    const url = screen.queryByText(/http:\/\/localhost:3003\/api\/blogs\/21/)
+    const likes = screen.queryByText(/35/)
+    expect(url).toBeDefined()
+    expect(likes).toBeDefined()
+
+  })
 })
