c5db23c798ba42909292abf7e9de9d0a1fd419a0
Alyx DARENNE <axda.web@gmail.com>
2023-05-12 14:31:30 +0200

    part5/ex18: Test log in + create new blog features

--- bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,10 +1,31 @@
 describe('Blog app', function() {
   beforeEach(function() {
-    cy.request('POST', 'http://localhost:3003/api/testing/reset')
-    cy.visit('http://localhost:3000')
+    cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
+    const user = {
+      name: 'Axda',
+      username: 'axda',
+      password: 'axda'
+    }
+    cy.request('POST', `${Cypress.env('BACKEND')}/users/`, user)
+    cy.visit('')
   })
 
   it('Login form is shown', function() {
     cy.contains('Log in to application')
+  })
+
+  describe('When logged in', function() {
+    beforeEach(function() {
+      cy.login({ username: 'axda', password: 'axda' })
+    })
+
+    it('A blog can be created', function() {
+      cy.createBlog({
+        title: 'My first blog with cypress',
+        author: 'Axda',
+        url: 'http://axda.com/blogs/1'
+      })
+      cy.contains('My first blog with cypress')
+    })
   })
 })
